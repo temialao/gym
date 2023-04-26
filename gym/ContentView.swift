81ba8476297df979aa1e1077a0907fe48ea3478f
Temi Alao <temialao12@gmail.com>
2023-04-26 10:37:06 +0100

    first commit

--- gym/ContentView.swift
+++ gym/ContentView.swift
@@ -1,85 +1,67 @@
-//
-//  ContentView.swift
-//  gym
-//
-//  Created by Temi Alao on 21/04/2023.
-//
-
 import SwiftUI
 import CoreData
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
 
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
+    @State private var username = ""
+    @State private var password = ""
+    @State private var wrongCredentials = false
+    @State private var isLoggedIn = false
 
     var body: some View {
         NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+            if isLoggedIn {
+                OnboardingView()
+            } else {
+                VStack {
+                    Text("Login or Signup")
+                        .font(.system(size: 40, weight: .bold, design: .rounded))
+                        .foregroundColor(.black)
+                        .padding()
+
+                    TextField("Username", text: $username)
+                        .padding()
+                        .background(Color.black.opacity(0.1))
+                        .cornerRadius(10)
+                        .frame(width: 300, height: 50, alignment: .center)
+
+                    SecureField("Password", text: $password)
+                        .padding()
+                        .background(Color.black.opacity(0.1))
+                        .cornerRadius(10)
+                        .frame(width: 300, height: 50, alignment: .center)
+
+                    if wrongCredentials {
+                        Text("Wrong username or password")
+                            .foregroundColor(.red)
+                            .padding(.bottom)
                     }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
+
+                    Button("Login or Signup") {
+                        authenticateUser(username: username, password: password)
                     }
+                    .foregroundColor(.white)
+                    .frame(width: 300, height: 50, alignment: .center)
+                    .background(Color.blue)
+                    .cornerRadius(10)
                 }
+                .navigationBarHidden(true)
             }
-            Text("Select an item")
         }
     }
 
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+    func authenticateUser(username: String, password: String) {
+        // Add your authentication logic here (e.g., checking with a database or server)
+        // For now, we'll assume a successful login if the username and password are not empty
+        if !username.isEmpty && !password.isEmpty {
+            isLoggedIn = true
+            wrongCredentials = false
+        } else {
+            wrongCredentials = true
         }
     }
 }
-
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
